d3d6fe453a250f2a48a7df958b87833842209d07
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-05-10 23:23:09 +0300

    Spring backend added

---
+++ backend/src/main/java/airflights/AirflightsApplication.java
@@ -0,0 +1,13 @@
+package airflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirflightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirflightsApplication.class, args);
+    }
+
+}
